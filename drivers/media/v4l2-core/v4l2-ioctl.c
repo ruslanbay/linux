b7e5280004233e93d27d03426c44111d756ec9f3
ruslanbay <67730802+ruslanbay@users.noreply.github.com>
2026-01-01 17:36:54 +0100

    add v4l2 from https://github.com/intel/linux-intel-lts/tree/lts-v5.15.195-android_t-251103T063840Z/drivers/media/

--- drivers/media/v4l2-core/v4l2-ioctl.c
+++ drivers/media/v4l2-core/v4l2-ioctl.c
@@ -16,6 +16,7 @@
 #include <linux/kernel.h>
 #include <linux/version.h>
 
+#include <linux/v4l2-subdev.h>
 #include <linux/videodev2.h>
 
 #include <media/v4l2-common.h>
@@ -282,7 +283,7 @@ static void v4l_print_format(const void *arg, bool write_only)
 	const struct v4l2_meta_format *meta;
 	u32 pixelformat;
 	u32 planes;
-	unsigned i;
+	unsigned int i;
 
 	pr_cont("type=%s", prt_names(p->type, v4l2_type_names));
 	switch (p->type) {
@@ -1019,7 +1020,7 @@ static void v4l_sanitize_format(struct v4l2_format *fmt)
 	/*
 	 * The v4l2_pix_format structure has been extended with fields that were
 	 * not previously required to be set to zero by applications. The priv
-	 * field, when set to a magic value, indicates the the extended fields
+	 * field, when set to a magic value, indicates the extended fields
 	 * are valid. Otherwise they will contain undefined values. To simplify
 	 * the API towards drivers zero the extended fields and set the priv
 	 * field to the magic value when the extended pixel format structure
@@ -1204,7 +1205,7 @@ static int v4l_enumoutput(const struct v4l2_ioctl_ops *ops,
 
 static void v4l_fill_fmtdesc(struct v4l2_fmtdesc *fmt)
 {
-	const unsigned sz = sizeof(fmt->description);
+	const unsigned int sz = sizeof(fmt->description);
 	const char *descr = NULL;
 	u32 flags = 0;
 
@@ -3090,6 +3091,22 @@ static int check_array_args(unsigned int cmd, void *parg, size_t *array_size,
 		ret = 1;
 		break;
 	}
+	case VIDIOC_SUBDEV_G_ROUTING:
+	case VIDIOC_SUBDEV_S_ROUTING: {
+		struct v4l2_subdev_routing *route = parg;
+
+		if (route->num_routes > 0) {
+			if (route->num_routes > 256)
+				return -EINVAL;
+
+			*user_ptr = (void __user *)route->routes;
+			*kernel_ptr = (void *)&route->routes;
+			*array_size = sizeof(struct v4l2_subdev_route)
+				    * route->num_routes;
+			ret = 1;
+		}
+		break;
+	}
 	}
 
 	return ret;
@@ -3286,7 +3303,7 @@ video_usercopy(struct file *file, unsigned int orig_cmd, unsigned long arg,
 		} else {
 			/* too big to allocate from stack */
 			mbuf = kmalloc(ioc_size, GFP_KERNEL);
-			if (NULL == mbuf)
+			if (mbuf == NULL)
 				return -ENOMEM;
 			parg = mbuf;
 		}
